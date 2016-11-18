4aadd4178b8e8a7c0cc931f8bc6e4473e4ad1424
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2016-11-18 16:19:30 -0200

    Cálculo

--- MvvmCrossPassaRegua/MvvmCrossPassaRegua.Droid/MainActivity.cs
+++ MvvmCrossPassaRegua/MvvmCrossPassaRegua.Droid/MainActivity.cs
@@ -1,13 +1,18 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using MvvmCrossPassaRegua;
 
 namespace MvvmCrossPassaRegua.Droid
 {
     [Activity(Label = "MvvmCrossPassaRegua.Droid", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        EditText txtValor;
+        SeekBar seekBarPessoas;
+        TextView lblPessoas;
+        TextView lblValorTotal;
+        TextView lblValorIndividual;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -19,8 +24,26 @@ namespace MvvmCrossPassaRegua.Droid
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
+            txtValor = FindViewById<EditText>(Resource.Id.txtValor);
+            seekBarPessoas = FindViewById<SeekBar>(Resource.Id.seekBarPessoas);
+            lblPessoas = FindViewById<TextView>(Resource.Id.lblPessoas);
+            lblValorTotal = FindViewById<TextView>(Resource.Id.lblValorTotal);
+            lblValorIndividual = FindViewById<TextView>(Resource.Id.lblResultado);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += (sender, e) => 
+            {
+                double valorTotal = 0;
+                double.TryParse(txtValor.Text, out valorTotal);
+                var pessoas = seekBarPessoas.Progress;
+
+                var calculadoraSerice = new CalculadoraService();
+
+                var valorIndividual = calculadoraSerice.CalculaIndividual(valorTotal, pessoas);
+
+                lblValorTotal.Text = $"R$ {valorTotal.ToString("0.00")}";
+                lblPessoas.Text = pessoas.ToString();
+                lblValorIndividual.Text = $"R$ {valorIndividual.ToString("0.00")}";
+            };
         }
     }
 }
---
+++ MvvmCrossPassaRegua/MvvmCrossPassaRegua/CalculadoraService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmCrossPassaRegua
+{
+    public class CalculadoraService
+    {
+        public virtual double CalculaIndividual(double total, int pessoas, bool dezPorcento = false)
+        {
+            if (pessoas == 0)
+                return 0;
+            else
+                return (total / pessoas) * (dezPorcento ? 1.1 : 1);
+        }
+    }
+}
